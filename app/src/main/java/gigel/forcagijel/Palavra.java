548f8fd6ada294d4aeece509edbeed064cb6696a
Gabriel da Silva Alves <gabrielonze@hotmail.com>
2016-05-15 13:18:19 -0300

    chute working class

--- app/src/main/java/gigel/forcagijel/Palavra.java
+++ app/src/main/java/gigel/forcagijel/Palavra.java
@@ -1,5 +1,7 @@
 package gigel.forcagijel;
 
+import android.util.Log;
+
 /**
  * Created by gabri on 01/05/2016.
  */
@@ -15,18 +17,28 @@ public class Palavra {
         palavra = novaPalavra;
         String p = "";
         for(int i = 0; i < novaPalavra.length(); i++){
-            p += " _".trim();
+            p += " _";
         }
         palavraDisplay = p.trim();
     }
 
     public String verificarChute(String chute){
         //TODO fazer code: Veriricar palavra e atualizar palavra display
+
+        String p = palavraDisplay;
+
+        for(int i = 0; i < palavra.length(); i++){
+            if(palavra.charAt(i) == chute.charAt(0)){
+                p = (p.substring(0, (i*2)) + chute + p.substring((i*2)+1, p.length()));
+            }
+        }
+        palavraDisplay = p;
+
         return palavraDisplay;
     }
 
     public String verificarTentativa(String tentativa){
         //TODO fazer code: Veriricar palavra e atualizar palavra display
-        return palavraDisplay;
+        return palavra;
     }
 }
--- app/src/main/java/gigel/forcagijel/Tela.java
+++ app/src/main/java/gigel/forcagijel/Tela.java
@@ -2,24 +2,29 @@ package gigel.forcagijel;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class Tela extends AppCompatActivity {
 
-    private Jogo jogo = new Jogo(this);
+    //private Jogo jogo = new Jogo(this);
+
+    private Palavra p1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.jogo_layout);
+        p1 = new Palavra("abacate");
     }
 
     public void avisarJogador(){
         //TODO avisar que é a vez do jogador
     }
 
-    public void clickChute(){
+    public void clickChute(View view){
         //TODO pegar texto tela
-        String textoTela = "";
-        jogo.jogador.Chutar(textoTela);
+        String a = p1.verificarChute("t");
+        //jogo.jogador.Chutar(textoTela);
     }
 }
