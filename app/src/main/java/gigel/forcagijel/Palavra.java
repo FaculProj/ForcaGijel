fcbf85e1ce1069f512d4fc0c3c931294eca93f18
Gabriel da Silva Alves <gabrielonze@hotmail.com>
2016-05-22 11:53:25 -0300

    Letras acentuadas

--- app/src/main/java/gigel/forcagijel/Palavra.java
+++ app/src/main/java/gigel/forcagijel/Palavra.java
@@ -3,6 +3,9 @@ package gigel.forcagijel;
 import android.util.Log;
 import android.widget.Toast;
 
+import java.text.Normalizer;
+import java.util.regex.Pattern;
+
 /**
  * Created by gabri on 01/05/2016.
  */
@@ -14,6 +17,8 @@ public class Palavra {
     //exemplo: A_A_A_E
     String palavraDisplay;
 
+    String palavraNormal;
+
     boolean palavraCompleta = true;
 
     public Palavra(String novaPalavra){
@@ -23,17 +28,25 @@ public class Palavra {
             p += " _";
         }
         palavraDisplay = p.trim();
+        palavraNormal = normalizar(palavra);
+    }
+
+    public static String normalizar(String str) {
+        String nfdNormalizedString = Normalizer.normalize(str, Normalizer.Form.NFD);
+        Pattern pattern = Pattern.compile("\\p{InCombiningDiacriticalMarks}+");
+        return pattern.matcher(nfdNormalizedString).replaceAll("");
     }
 
     public RespostaPalavra verificarChute(String chute){
         boolean acertou = false;
         String p = palavraDisplay;
 
+        chute = normalizar(chute);
         if(! chute.equals("")) {
-            for (int i = 0; i < palavra.length(); i++) {
-                if (palavra.toLowerCase().charAt(i) == chute.toLowerCase().charAt(0)) {
+            for (int i = 0; i < palavraNormal.length(); i++) {
+                if (palavraNormal.toLowerCase().charAt(i) == chute.toLowerCase().charAt(0)) {
                     acertou = true;
-                    p = (p.substring(0, (i * 2)) + chute + p.substring((i * 2) + 1, p.length()));
+                    p = (p.substring(0, (i * 2)) + palavra.toUpperCase().charAt(i) + p.substring((i * 2) + 1, p.length()));
                 }
             }
         }
@@ -59,7 +72,9 @@ public class Palavra {
     public RespostaPalavra verificarTentativa(String tentativa){
         RespostaPalavra r;
 
-        if(tentativa.toLowerCase().trim().equals(palavra.toLowerCase())){
+        tentativa = normalizar(tentativa);
+
+        if(tentativa.toLowerCase().trim().equals(palavraNormal.toLowerCase()) ){
             palavraDisplay = palavra;
             palavraCompleta = true;
             r = new RespostaPalavra(palavra, true);
